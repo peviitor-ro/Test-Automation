29631635ff21a99afe177849bcff326ae48077a0
GeorgeBelu <george_belu@epam.com>
2023-02-26 13:17:09 +0200

    removed unused class in BasePage

--- src/main/java/ro/peviitor/pageobject/BasePage.java
+++ src/main/java/ro/peviitor/pageobject/BasePage.java
@@ -1,23 +1,17 @@
 package ro.peviitor.pageobject;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import ro.peviitor.drivermanager.DriverManager;
 
 import java.time.Duration;
 
-import static ro.peviitor.utils.TypesOfData.*;
-
 
 public abstract class BasePage
 {
     protected WebDriver webDriver;
-
     protected final WebDriverWait wait;
 
     protected BasePage()
@@ -26,20 +20,4 @@ public abstract class BasePage
         this.wait = new WebDriverWait(webDriver, Duration.ofSeconds(20));
         PageFactory.initElements(new AjaxElementLocatorFactory(webDriver, 20), this);
     }
-
-    protected WebElement getElementBy(String byType, String path, int time)
-    {
-        switch (byType)
-        {
-            case ID:
-                return new WebDriverWait(webDriver, Duration.ofSeconds(time)).until(ExpectedConditions.visibilityOfElementLocated(By.id(path)));
-            case NAME:
-                return new WebDriverWait(webDriver, Duration.ofSeconds(time)).until(ExpectedConditions.visibilityOfElementLocated(By.name(path)));
-            case XPATH:
-                return new WebDriverWait(webDriver, Duration.ofSeconds(time)).until(ExpectedConditions.visibilityOfElementLocated(By.xpath(path)));
-            default:
-                return null;
-        }
-    }
-
 }
